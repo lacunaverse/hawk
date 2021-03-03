8275b45a08788751a39cbf0e23fed0bc3d123d41
hvlck <just.ethan@protonmail.com>
2021-03-02 22:07:36 -0500

    chore: remove old, begin go rewrite

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hvlck/hawk
+
+go 1.16
+
+require github.com/gorilla/mux v1.8.0
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Templates
+type Templates struct {
+	index  *template.Template
+	errors *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, cat string) error {
+	switch cat {
+	case "index":
+		return t.index.ExecuteTemplate(w, name, data)
+	case "errors":
+		return t.errors.ExecuteTemplate(w, name, data)
+	default:
+		return t.errors.ExecuteTemplate(w, name, data)
+	}
+}
+
+type NotFound struct {
+}
+
+func (n NotFound) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.Render(w, "404.html", "", "errors")
+}
+
+var t = &Templates{
+	index:  template.Must(template.ParseFiles("views/layout.html", "views/index.html", "views/layouts/nav.html")),
+	errors: template.Must(template.ParseFiles("views/layout.html", "views/errors/404.html", "views/layouts/nav.html")),
+}
+
+// Index root route
+func Index(w http.ResponseWriter, r *http.Request) {
+	t.Render(w, "index.html", "", "index")
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/dist")))).Methods("GET")
+
+	r.HandleFunc("/", Index).Methods("GET")
+
+	r.NotFoundHandler = NotFound{}
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
